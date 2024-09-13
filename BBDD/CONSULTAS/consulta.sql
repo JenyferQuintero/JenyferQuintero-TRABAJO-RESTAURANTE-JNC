ce2bf9de249de99d06a0641de5fe5c403f35bc84
JenyferQuintero <jefer1319@gmail.com>
2024-09-13 15:21:49 -0500

    trabajo restaurante

---
+++ BBDD/CONSULTAS/consulta.sql
@@ -0,0 +1,10 @@
+SELECT * from orden;
+
+SELECT * FROM mesero;
+
+SELECT fecha_hora, mesa FROM orden where id_orden=30;
+
+SELECT * from tipo_producto;
+
+SELECT producto_nombre FROM producto WHERE id_producto=30;
+
