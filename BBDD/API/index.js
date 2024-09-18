5bc25d3bba3e83244c829813747234768eea3aff
JenyferQuintero <jefer1319@gmail.com>
2024-09-18 16:18:19 -0500

    se deja configurado fecha y hora, y se inicia orden de compra y sincronizacion de BBDD

--- BBDD/API/index.js
+++ BBDD/API/index.js
@@ -1,58 +1,82 @@
-const path=require('path')
-const express=require('express')
+const path = require('path')
+const express = require('express')
 const body_Parser = require('body-parser');
-const mysql2=require('mysql2/promise');
+const mysql2 = require('mysql2/promise');
 const { error } = require('console');
 
 
 
-const app=express()
-const puerto=3000
+const app = express()
+const puerto = 3000
 
 app.use(express.static('public'));
 app.use(express.json())
-app.use(express.urlencoded({extended:true})) 
-app.use(body_Parser.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
+app.use(body_Parser.urlencoded({ extended: true }))
 
 app.use(body_Parser.json());
 
 
-console.log(path.join(__dirname,'../PUBLIC'))
-app.use('/',express.static(path.join(__dirname,'../PUBLIC')))
+console.log(path.join(__dirname, '../PUBLIC'))
+app.use('/', express.static(path.join(__dirname, '../PUBLIC')))
 
 
-app.listen(puerto,()=>{
-    console.log('Servidor Iniciado')
+app.listen(puerto, () => {
+  console.log('Servidor Iniciado')
 })
-const db=mysql2.createPool({
-    user:'root',
-    database:'BBDD_RESTAURANTE',
-    host:'localhost',
-    password:''
+const db = mysql2.createPool({
+  user: 'root',
+  database: 'BBDD_RESTAURANTE',
+  host: 'localhost',
+  password: ''
 
 })
 
-app.post('/login',async(req,res)=>{
-    const{nombre_mesero, contrasena}=req.body
- console.log(nombre_mesero, contrasena)
-    try{
-        const contrasena1=parseInt(contrasena)
-        const [filas]=await db.query('SELECT * FROM MESERO WHERE NOMBRE_MESERO=? AND CONTRASENA=?',[nombre_mesero, contrasena1])
-      if(filas.length > 0){
-        res.sendFile(path.join(__dirname,'../PUBLIC/indexorden.html')) 
-      } else {
-        res.status(401).json({Error: 'Inicio de Sesión Incorrecta'})
-
-        res.sendFile(path.join(__dirname,'../PUBLIC/errorinicio.html')) 
-       
+app.post('/login', async (req, res) => {
+  const { nombre_mesero, contrasena } = req.body
+  console.log(nombre_mesero, contrasena)
+  try {
+    const contrasena1 = parseInt(contrasena)
+    const [filas] = await db.query('SELECT * FROM MESERO WHERE NOMBRE_MESERO=? AND CONTRASENA=?', [nombre_mesero, contrasena1])
+    if (filas.length > 0) {
+      res.sendFile(path.join(__dirname, '../PUBLIC/indexorden.html'))
+    } else {
+      res.sendFile(path.join(__dirname, '../PUBLIC/errorinicio.html'))
+     
       }
-      
 
-    }
-    catch(error){
-        console.error('Error de datos', error)
-    }
+ }
+  catch (error) {
+    console.error('Error de datos', error)
+  }
 })
+
+/*orden*/
+
+app.post('/agregar-producto',async (req, res) => {
+  const { producto_nombre,precio } = req.body;
+  console.log(producto_nombre, precio)
+  try {
+   
+    const [filas] = await db.query('SELECT * FROM PRODUCTO WHERE PRODUCTO_NOMBRE=? AND PRECIO=?', [producto_nombre, precio])
+    console.log(rows)
+    res.send('Oden de compra generada con exito');
+      }
+      catch(error){
+          console.error('Error de datos', error)
+      }
+
+}) 
+
+
+
+
+
+  
+
+
+
+
 
 
 
